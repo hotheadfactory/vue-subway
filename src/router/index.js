f6fdb99631cab50d52cdc5b8f2629db0586665a6
Andy H. Jung <commaniakr@gmail.com>
2020-07-13 19:31:01 +0900

    feat: 구간 탭 추가

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Index from "../components/Index.vue";
 import AdminStation from "../components/AdminStation.vue";
 import AdminLine from "../components/AdminLine.vue";
+import AdminEdge from "../components/AdminEdge.vue";
 
 Vue.use(VueRouter);
 
@@ -12,5 +13,6 @@ export const router = new VueRouter({
     { path: "/", component: Index },
     { path: "/admin-station", component: AdminStation },
     { path: "/admin-line", component: AdminLine },
+    { path: "/admin-edge", component: AdminEdge },
   ],
 });
